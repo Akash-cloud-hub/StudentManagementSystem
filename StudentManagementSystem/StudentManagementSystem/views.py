291b31321c90eef26271ae1a7f4f43b93126f90a
Akash <airshula79@gmail.com>
2023-12-23 22:25:56 +0700

    Created Index and module

--- StudentManagementSystem/StudentManagementSystem/views.py
+++ StudentManagementSystem/StudentManagementSystem/views.py
@@ -11,5 +11,18 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.db'
 
 db = SQLAlchemy(app)
 
-#class Student(db.Model):
-  
+class Student(db.Model):
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(100),nullable = False) #nullable  = False meaning no values cannot be stored , if user enters nothing it is not stored
+    age = db.Column(db.Integer,nullable = False)
+
+
+@app.route('/')
+def index():
+    students = Student.query.all()
+    return render_template('index.html', students = students)
+
+if __name__=='__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
